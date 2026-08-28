a43263ace3ed7afd9f08f696516d367889317a2b
Carlos Eduardo <eduardo938soares@gmail.com>
2026-08-28 13:27:37 +0000

    Projeto Finalizado-Eduardo

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,8 +1,40 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+const cliente="Mariana Silva"
+const cidade="Fortaleza "
+const produto="Notebook pro"
+const categoria="Notebook"
+const preco="3500"
+const quantidade="2"
+const descontoPercentual="10"
+const valorPago="7000"
+const subtotal=preco*quantidade
+const valorDesconto=subtotal * descontoPercentual/100
+const valorFinal=subtotal-valorDesconto
+const troco=valorPago-valorFinal
+const resumo=` 
+=========================================
+ TECHSTORE SISTEMAS
+ RESUMO DO PEDIDO
+=========================================
 
+cliente: R$ ${cliente}
+cidade: R$ ${cidade}
+produto: R$ ${produto}
+categoria: R$ ${categoria}
+preco: R$ ${preco}
+quantidade: R$ ${quantidade}
+descontPercentual:R$ ${descontoPercentual}
+valorPago: R$ ${valorPago}
+subtotal: R$ ${subtotal}
+valorDeconsto: R$ ${valorDesconto}
+valorFinal: R$ ${valorFinal}
+troco: R$ ${troco}
+=========================================
+=========================================
+`
 
-
+console.log(resumo)
 
 
 // === FIM DO CÓDIGO =======================
